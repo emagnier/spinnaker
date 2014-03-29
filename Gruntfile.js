7219f21527ef82d25d87074fc1da6e46c791768b
Etienne Magnier <etienne.magnier@gmail.com>
2014-03-29 17:03:37 -0400

    Add JsHint task + the jshint-stylish, time-grunt and load-grunt-tasks modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,69 +13,75 @@
  */
 
 module.exports = function(grunt) {
+  'use strict';
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Load the include-all library in order to require all of our grunt
+  // configurations and task registrations dynamically.
+  var includeAll;
+  try {
+    includeAll = require('include-all');
+  } catch (e0) {
+    try {
+      includeAll = require('sails/node_modules/include-all');
+    }
+    catch (e1) {
+      console.error('Could not find `include-all` module.');
+      console.error('Skipping grunt tasks...');
+      console.error('To fix this, please run:');
+      console.error('npm install include-all --save`');
+      console.error();
+
+      grunt.registerTask('default', []);
+      return;
+    }
+  }
 
 
-	// Load the include-all library in order to require all of our grunt
-	// configurations and task registrations dynamically.
-	var includeAll;
-	try {
-		includeAll = require('include-all');
-	} catch (e0) {
-		try {
-			includeAll = require('sails/node_modules/include-all');
-		}
-		catch(e1) {
-			console.error('Could not find `include-all` module.');
-			console.error('Skipping grunt tasks...');
-			console.error('To fix this, please run:');
-			console.error('npm install include-all --save`');
-			console.error();
+  /**
+   * Loads Grunt configuration modules from the specified
+   * relative path. These modules should export a function
+   * that, when run, should either load/configure or register
+   * a Grunt task.
+   */
+  function loadTasks(relPath) {
+    return includeAll({
+      dirname: require('path').resolve(__dirname, relPath),
+      filter: /(.+)\.js$/
+    }) || {};
+  }
 
-			grunt.registerTask('default', []);
-			return;
-		}
-	}
-
-
-	/**
-	 * Loads Grunt configuration modules from the specified
-	 * relative path. These modules should export a function
-	 * that, when run, should either load/configure or register
-	 * a Grunt task.
-	 */
-	function loadTasks(relPath) {
-		return includeAll({
-			dirname: require('path').resolve(__dirname, relPath),
-			filter: /(.+)\.js$/
-		}) || {};
-	}
-
-	/**
-	 * Invokes the function from a Grunt configuration module with
-	 * a single argument - the `grunt` object.
-	 */
-	function invokeConfigFn(tasks) {
-		for (var taskName in tasks) {
-			if (tasks.hasOwnProperty(taskName)) {
-				tasks[taskName](grunt);
-			}
-		}
-	}
+  /**
+   * Invokes the function from a Grunt configuration module with
+   * a single argument - the `grunt` object.
+   */
+  function invokeConfigFn(tasks) {
+    for (var taskName in tasks) {
+      if (tasks.hasOwnProperty(taskName)) {
+        tasks[taskName](grunt);
+      }
+    }
+  }
 
 
+  // Load task functions
+  var taskConfigurations = loadTasks('./tasks/config'),
+    registerDefinitions = loadTasks('./tasks/register');
 
+  // (ensure that a default task exists)
+  if (!registerDefinitions.default) {
+    registerDefinitions.default = function(grunt) {
+      grunt.registerTask('default', []);
+    };
+  }
 
-	// Load task functions
-	var taskConfigurations = loadTasks('./tasks/config'),
-		registerDefinitions = loadTasks('./tasks/register');
-
-	// (ensure that a default task exists)
-	if (!registerDefinitions.default) {
-		registerDefinitions.default = function (grunt) { grunt.registerTask('default', []); };
-	}
-
-	// Run task functions to configure Grunt.
-	invokeConfigFn(taskConfigurations);
-	invokeConfigFn(registerDefinitions);
+  // Run task functions to configure Grunt.
+  invokeConfigFn(taskConfigurations);
+  invokeConfigFn(registerDefinitions);
 
 };
---
+++ tasks/config/jshint.js
@@ -0,0 +1,20 @@
+/**
+ * Validate files with JSHint.
+ */
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.config.set('jshint', {
+    options: {
+      jshintrc: '.jshintrc',
+      reporter: require('jshint-stylish')
+    },
+    files: [
+      'Gruntfile.js',
+      'tasks/**/*.js',
+      'api/{,*/}*.js',
+      'assets/**/*.js',
+      '!assets/**/*.io.js'
+    ]
+  });
+};
